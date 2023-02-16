ed0018ca38c18d0caa52c26c0ed6453825b2b3e4
tead1234 <tead1234@gmail.com>
2023-02-16 22:40:20 +0900

    queryRepositort함수가 자꾸 스태틱으로 걸리네

--- src/main/java/com/example/analyzingatopyexternalFactors/repository/SymtomQueryRepository.java
+++ src/main/java/com/example/analyzingatopyexternalFactors/repository/SymtomQueryRepository.java
@@ -2,6 +2,8 @@ package com.example.analyzingatopyexternalFactors.repository;
 
 import com.example.analyzingatopyexternalFactors.entity.SymEntity;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,7 @@ import java.util.List;
 // 메소드 구현 레포
 // JPA와 위 인터페이스를 상속받는
 @Repository
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class SymtomQueryRepository {
     private final JPAQueryFactory queryFactory;
@@ -19,14 +22,9 @@ public class SymtomQueryRepository {
                 .fetch();
     }
 
-    public List<SymEntity> findByUsername(String username) {
-        return queryFactory
-                .selectFrom(symentity)
-                .where(symentity.username.eq(username))
-                .fetch();
-    }
+
     // 피부상태가 4 또는 5를 기록하는 날의 영향을 주는 음식, 식단, 운동 상태
-    public static List<Object> findMostFactors(String category){
+    public List<Object> findMostFactors(String category){
         return queryFactory.selectFrom(symentity)
                 .groupBy(symentity.skinState)
                 .fetch();
