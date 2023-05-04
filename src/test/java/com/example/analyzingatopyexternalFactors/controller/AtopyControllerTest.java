defc9c2e27b8c1bc8a2ca170a001e501335bee6a
tead1234 <tead1234@gmail.com>
2023-05-04 23:51:11 +0900

    make test and db

---
+++ src/test/java/com/example/analyzingatopyexternalFactors/controller/AtopyControllerTest.java
@@ -0,0 +1,40 @@
+package com.example.analyzingatopyexternalFactors.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AtopyControllerTest {
+
+    @Test
+    void show() throws JsonProcessingException {
+
+    }
+
+    @Test
+    void showById() {
+        AtopyController atopy = new AtopyController();
+//        assertArrayEquals(atopy.showById(1),  );
+    }
+
+    @Test
+    void showByCategory() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+}
