398ca52cab47d133ee51ba114373ab87e53dd1fa
tead1234 <tead1234@gmail.com>
2023-05-05 22:49:50 +0900

    member domain create

---
+++ src/main/java/com/example/analyzingatopyexternalFactors/Member/entity/MemberEntity.java
@@ -0,0 +1,9 @@
+package com.example.analyzingatopyexternalFactors.Member.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "member_entity")
+public class MemberEntity {
+}
